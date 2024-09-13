d62feb203c1fa2f770526747d2103a1a4653c5d3
gowdasagar06 <sagargowda6666@gmail.com>
2024-09-13 17:30:25 +0530

    push

---
+++ aws/infra/codepipeline/application_start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Assuming Tomcat is installed in /opt/apache-tomcat-10.1.18, adjust the path accordingly
+TOMCAT_HOME="/opt/apache-tomcat-9.0.93/"
+ 
+sudo $TOMCAT_HOME/bin/shutdown.sh
+sudo rm -rf $TOMCAT_HOME/webapps/*
+sudo mv /tmp/cas-scheduler.war /tmp/ROOT.war
+sudo cp -R /tmp/*.war $TOMCAT_HOME/webapps
+sudo $TOMCAT_HOME/bin/startup.sh
+
+# Wait for Tomcat to fully stop (adjust the sleep duration based on your application's shutdown time)
+# sleep 10
+
+# Start Tomcat
+# sudo $TOMCAT_HOME/bin/startup.sh
+
+# Wait for Tomcat to fully start (adjust the sleep duration based on your application's startup time)
+# sleep 30
+
+# Additional commands or configurations after Tomcat has started can be added here
+# For example, you might want to tail the catalina.out log file for debugging:
+# tail -f $TOMCAT_HOME/logs/catalina.out
