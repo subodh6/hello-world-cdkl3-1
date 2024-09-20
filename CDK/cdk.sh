3ae054905a5e6ad933fd555bfa7e289477ae5fb2
gowdasagar06 <sagargowda6666@gmail.com>
2024-09-20 17:45:28 +0530

    push

--- CDK/cdk.sh
+++ CDK/cdk.sh
@@ -1,7 +1,8 @@
 curl -sL https://rpm.nodesource.com/setup_20.x | sudo bash -
 sudo dnf install -y nodejs
 sudo npm install -g typescript
+npm install aws-cdk
 npm install aws-cdk-lib
 npm install constructs
-cdk synth
-cdk deploy
+npx cdk synth
+npx cdk deploy
