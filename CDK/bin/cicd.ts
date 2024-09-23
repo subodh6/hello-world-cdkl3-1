5c573636fb667ff2278f0c27a71e7d437a572ab3
subodh6 <46165221+subodh6@users.noreply.github.com>
2024-09-23 15:36:01 +0530

    Update cicd.ts

--- CDK/bin/cicd.ts
+++ CDK/bin/cicd.ts
@@ -8,6 +8,6 @@ const app = new cdk.App();
 //   env: { account: '637423476845', region: 'ap-south-1' },
 // });
 
-new crossaccount(app, 'cas-scheduler-admin1', {
+new crossaccount(app, 'cas-scheduler-admin-l3', {
   env: { account: '954503069243', region: 'us-east-1' },
 });
